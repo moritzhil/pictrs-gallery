752152f3c777e3392e7721020bb9b4c9f0d6e668
moritzhil <moritz.hilpert@outlook.com>
2025-04-09 13:24:29 +0200

    Update pictrs_scraper.py

--- pictrs_scraper.py
+++ pictrs_scraper.py
@@ -18,31 +18,31 @@ def fetch_galerien():
     # Galerien-Daten sammeln
     galerien = []
 
-    # Alle Galerien finden (Hier ist davon auszugehen, dass die Galerien Links sind)
-    gallery_links = soup.find_all('a', class_='gallery-link-class')  # Passen Sie die Klasse an, falls sie anders ist
-    
+    # Alle Galerien finden
+    gallery_links = soup.find_all('a', class_='albums-grid-item')  # Hier die richtige Klasse für die Galerie-Links
+    print(f"Gefundene Galerien: {len(gallery_links)}")
+
     for gallery_link in gallery_links:
-        galerie_slug = gallery_link.get('href').split('/')[-1]  # Extrahiert den Slug der Galerie
-        galerie_titel = gallery_link.get_text().strip()  # Titel der Galerie
+        galerie_slug = gallery_link.get('href').split('/')[-2]  # Extrahiert den Slug der Galerie aus der URL
+        galerie_titel = gallery_link.find('span', class_='albums-grid-title').get_text().strip()  # Titel der Galerie
+        galerie_bilder_count = gallery_link.find('small', class_='albums-grid-details').get_text().strip()  # Anzahl der Bilder
 
         # Galerie-Daten in ein Dictionary speichern
         galerie = {
             'slug': galerie_slug,
             'titel': galerie_titel,
-            'bilder': []
+            'bilder': [],
+            'bilder_count': galerie_bilder_count  # Anzahl der Bilder
         }
 
-        # Jedes Bild aus der Galerie extrahieren
-        bild_tags = gallery_link.find_all('img')
-
-        for bild_tag in bild_tags:
-            bild_url = bild_tag.get('src')  # Bild-URL extrahieren
-            bild_link = f"https://www.pictrs.com{bild_url}"  # Vollständige URL des Bildes
-            
-            galerie['bilder'].append({
-                'bild': bild_link,
-                'link': f'https://deinshop.pictrs.com/galerie/{galerie_slug}/{bild_url.split("/")[-1]}'
-            })
+        # Das Hintergrundbild des <div> mit der Klasse 'albums-grid-image' extrahieren
+        bild_url = gallery_link.find('div', class_='albums-grid-image').get('style').split('url(')[-1].split(')')[0]  # Bild-URL extrahieren
+        bild_link = f"https://www.pictrs.com{bild_url}"  # Vollständige URL des Bildes
+        
+        galerie['bilder'].append({
+            'bild': bild_link,
+            'link': f'https://deinshop.pictrs.com/galerie/{galerie_slug}/{bild_url.split("/")[-1]}'
+        })
 
         # Die Galerie zur Liste hinzufügen
         galerien.append(galerie)
