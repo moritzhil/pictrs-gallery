993018c73367ed09c23289862fb406dfb08dac5c
moritzhil <moritz.hilpert@outlook.com>
2025-04-10 16:41:28 +0200

    Update pictrs-scraper-gallery.py

--- pictrs-scraper-gallery.py
+++ pictrs-scraper-gallery.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time
 
@@ -17,8 +20,18 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 url = "https://www.pictrs.com/moritz-hilpert/9528141/see?l=de"
 driver.get(url)
 
-# Warte, bis die Seite vollständig geladen ist (ggf. anpassen)
-time.sleep(5)
+# Warte, bis die Seite vollständig geladen ist (Warte auf das erste Bild-Tag)
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "picthumbs")))
+
+# Optional: Scrollen, um sicherzustellen, dass alle Bilder geladen sind
+last_height = driver.execute_script("return document.body.scrollHeight")
+while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)  # Gib der Seite Zeit zum Nachladen
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
 
 # HTML der Seite extrahieren
 html = driver.page_source
@@ -27,12 +40,13 @@ driver.quit()
 # HTML parsen
 soup = BeautifulSoup(html, "html.parser")
 
-# Finde alle Bild-Container
+# Finde alle Bild-Container (Achte auf richtige Selektoren!)
 image_items = soup.select("span.imageitem")
 
 bilder = []
 
 for item in image_items:
+    # Extrahiere relevante Daten
     data_id = item.get("data-id")
     a_tag = item.find("a", class_="thumba")
     img_tag = item.find("img", class_="picthumbs")
